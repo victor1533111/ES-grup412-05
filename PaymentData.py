def00f77007ef05dbff37f8615de7d727d060a89
cristiangutierrz <cristian.gutierrezg@e-campus.uab.cat>
2020-05-14 14:03:42 +0200

    Constructores

--- PaymentData.py
+++ PaymentData.py
@@ -1,4 +1,7 @@
 class PaymentData:
 
-    def __init__(self):
+    def __init__(self, titular, num_tarjeta, cvc):
+        self.titular = titular
+        self.num_tarjeta = num_tarjeta
+        self.cvc = cvc
         pass
--- User.py
+++ User.py
@@ -1,4 +1,9 @@
 class User:
 
-    def __init__(self):
+    def __init__(self, name, DNI, direccion, telf, email):
+        self.name = name
+        self.DNI = DNI
+        self.direccion = direccion
+        self.telf = telf
+        self.email = email
         pass
