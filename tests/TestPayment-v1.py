deeb81d9b9c4bf412b6895b925be2821bb891f5b
victor1533111 <cuentaplay7778@gmail.com>
2020-05-16 18:08:33 +0200

    vs code

--- tests/TestPayment-v1.py
+++ tests/TestPayment-v1.py
@@ -1,1 +1,5 @@
-import unitestclass TestPayment_v1(unitest.TestCase):    def validate_payment_method(self):        # assert VISA o MASTERCARD
+import unitest
+
+class TestPayment_v1(unitest.TestCase):
+    def validate_payment_method(self):
+        # assert VISA o MASTERCARD
