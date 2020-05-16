74c24cbf1797f045fa2e7f3a8984a623ee19da54
cristiangutierrz <cristian.gutierrezg@e-campus.uab.cat>
2020-05-16 17:16:30 +0200

    arreglat constructor payment data

--- PaymentData.py
+++ PaymentData.py
@@ -1,7 +1,12 @@
 class PaymentData:
 
-    def __init__(self, titular, num_tarjeta, cvc):
+    def __init__(self, titular, num_tarjeta, cvc, method, import_):
         self.titular = titular
         self.num_tarjeta = num_tarjeta
         self.cvc = cvc
+        self.method = method
+        self.import_ = import_
         pass
+    
+    def validar_datos(self):
+        return True
