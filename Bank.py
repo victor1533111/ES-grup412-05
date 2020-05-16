a8b18656108a41d7b82d88de01ea6902bf2fe524
cristiangutierrz <cristian.gutierrezg@e-campus.uab.cat>
2020-05-16 17:03:01 +0200

    Realizar pago - V1

--- Bank.py
+++ Bank.py
@@ -8,4 +8,8 @@ class Bank:
         pass
 
     def do_payment(self, user: User, payment_data: PaymentData):
+        while PaymentData.validar_datos(payment_data) == False:
+            PaymentData.pedir_datos()
+        else:
+            
         return True
