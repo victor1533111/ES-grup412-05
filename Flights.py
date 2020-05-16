68905dae6466567318ec70b508d94f388e62d5e9
moasi <moasi3108@users.noreply.github.com>
2020-05-16 16:00:28 +0200

    funcion init hecha

--- Flights.py
+++ Flights.py
@@ -1,34 +1,36 @@
 class Flights:
 
-    def __init__(self, viajeros,origen,destinos):
-        if(viajeros>0):
-            self.viajeros=viajeros
-        else:
+    def __init__(self, viajeros,origen,destinos, precio_vuelo):
+        if(viajeros<1):
             print("El numero de viajeros es incorrecto, debe ser superior a uno.")
-        if(len(destinos)==0):
-            self.destinos=[]
-            self.vuelos=[]
-            self.precio=0
         else:
-            self.destinos=destinos
-            list(self.destinos)
-            self.vuelos=[]
-            self.vuelos.append([origen,self.destinos[0]])
-            if len(destinos)!=1:                
-                i=0
-                for dest in self.destinos:
-                    if dest != self.destinos[-1]:
-                        self.vuelos.append([dest,self.destinos[i+1]])
+            if(len(destinos)==0):
+                self.destinos=[]
+                self.vuelos=[]
+                self.precio_total=0
+            else:
+                self.destinos=destinos
+                list(self.destinos)
+                self.vuelos=[]
+                self.vuelos.append([origen,self.destinos[0]])
+                if len(destinos)!=1:                
+                    i=0
+                    for dest in self.destinos:
+                        if dest != self.destinos[-1]:
+                            self.vuelos.append([dest,self.destinos[i+1]])
 
-                    i=i+1
-            self.vuelos.append([self.destinos[-1],origen])
+                        i=i+1
+                self.vuelos.append([self.destinos[-1],origen])
+                self.precio_vuelos=[]
+                self.precio_vuelos.append(precio_vuelo)
+                for pre in precio_vuelos:
+                    self.precio_total+=pre
 
-            print("Falta actualizar precio")
 
         pass
 
 
-    def AñadirDestino(self,Ndestino,posicion):
+    def AñadirDestino(self,Ndestino,posicion, precio_destino):
         NuevosDestinos=[]
         i=0
         for dest in self.destinos:
