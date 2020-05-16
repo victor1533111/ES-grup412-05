ba1974ab46dab9289458550c8fa5bab29bd7fc25
victor1533111 <cuentaplay7778@gmail.com>
2020-05-16 21:05:00 +0200

    test user

--- tests/TestUser/testUser.py
+++ tests/TestUser/testUser.py
@@ -1,10 +1,15 @@
-from ../.. import Flights
-from ../.. import Users
+from src.Flights import Flights
+from src.User import User
 
 
-class testUser:
+
+class testUser():
+
     def test_gestiopassatgers(self):
-        usuario = User("Ruben","4712458T","Calle Vic","645548572","rubenjibo@gmail.com")
-        numero = 7
-        assert numero == Flights.viajeros
-        assert user == Flights.usuario
+        self.usuario= User("Ruben","4712458T","Calle Vic","645548572","rubenjibo@gmail.com")
+        numeroviajeros=5
+        self.vuelo = Flights(numeroviajeros,self.usuario,"Valencia","Barcelona",200)
+
+        assert numeroviajeros == self.vuelo.viajeros
+        assert self.usuario == self.vuelo.usuario
+        
