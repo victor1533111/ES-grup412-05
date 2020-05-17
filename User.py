feb4e29455b52e3259c439c6ded1a9f420d8a89f
moasi <moasi3108@users.noreply.github.com>
2020-05-17 17:12:17 +0200

    test lista vacia

---
+++ User.py
@@ -0,0 +1,21 @@
+class User:
+
+    def __init__(self, name, DNI, direccion, telf, email):
+        self.name = name
+        self.DNI = DNI
+        self.direccion = direccion
+        self.telf = telf
+        self.email = email
+        pass
+
+    def Trip(self, numpass, origen, listavuelos, hotel_list, car_list, fecha_in, fecha_f, precio_tot):
+        self.viajeros=numpass
+        self.origen=origen
+        self.lista_vuelos=listavuelos
+        self.hotel_list
+        self.car_list
+        self.fecha_inicio=fecha_in
+        self.fecha_final=fecha_f
+        self.precio_total=precio_tot
+
+        pass
--- tests/TestFlights/testFlights.py
+++ tests/TestFlights/testFlights.py
@@ -2,14 +2,21 @@ from src.Flights import Flights
 from src.User import User
 
 
-
 class testUser():
 
     def test_gestiopassatgers(self):
-        self.usuario= User("Ruben","4712458T","Calle Vic","645548572","rubenjibo@gmail.com")
-        numviajeros=5
-        self.vuelo = Flights(numviajeros,self.usuario,"Valencia","Barcelona",200)
+        self.usuario = User("Ruben", "4712458T", "Calle Vic",
+                            "645548572", "rubenjibo@gmail.com")
+        numviajeros = 5
+        self.vuelo = Flights(numviajeros, self.usuario,
+                             "Valencia", "Barcelona", 200)
 
         assert numviajeros == self.vuelo.viajeros
         assert self.usuario == self.vuelo.usuario
-        
+
+    def test_listavacia(self):
+        self.usuario = User("Ruben", "4712458T", "Calle Vic",
+                            "645548572", "rubenjibo@gmail.com")
+        numviajeros = 1
+        self.viaje = Flights(numviajeros,self.usuario)
+        assert self.viaje.vuelos==[]
