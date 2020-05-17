5526a375be712f9aeb5e4d623a7a21f2948a75dd
rubenjibo <65294442+rubenjibo@users.noreply.github.com>
2020-05-17 18:04:48 +0200

    test Flight actualizado

--- tests/TestFlights/testFlights.py
+++ tests/TestFlights/testFlights.py
@@ -51,6 +51,38 @@ class testFlights(unittest.TestCase):
         self.assertListEqual(vuelo.vuelos, expected)
         self.assertListEqual(vuelo.destinos, expected)
 
+    def test_gestiopassatgers(self):
+        self.usuario= User("Ruben","4712458T","Calle Vic","645548572","rubenjibo@gmail.com")
+        numeroviajeros=5
+        destinos=["Valencia","Madrid"]
+        self.vuelo = Flights(numeroviajeros,self.usuario,destinos,"Barcelona",200)
+
+        assert numeroviajeros == self.vuelo.viajeros
+        assert self.usuario == self.vuelo.usuario
+    
+    def test_AñadirDestino(self):
+            usuario= User("Ruben","4712458T","Calle Vic","645548572","rubenjibo@gmail.com")
+            destinos=["Valencia","Madrid"]
+            Flights(5,usuario,"Barcelona",destinos,200)
+            Flights.AñadirDestino("Amsterdam",2,10)
+            DestinosEsperados=["Valencia","Amsterdam","Madrid"]
+            VuelosEsperados=[["Barcelona","Valencia"],["Valencia","Amsterdam"],["Amsterdam","Madrid"],["Madrid","Barcelona"]]
+            precioEsperado=4000
+            assert precioEsperado == Flights.precio_total
+            assert DestinosEsperados == Flights.destinos
+            assert VuelosEsperados == Flights.vuelos
+
+    def test_BorrarDestino(self):
+            usuario= User("Ruben","4712458T","Calle Vic","645548572","rubenjibo@gmail.com")
+            destinos=["Valencia","Amsterdam","Madrid"]
+            Flights(5,usuario,"Barcelona",destinos,200)
+            Flights.BorrarDestino("Amsterdam")
+            DestinosEsperados=["Valencia","Madrid"]
+            VuelosEsperados=[["Barcelona","Valencia"],["Valencia","Madrid"],["Madrid","Barcelona"]]
+            precioEsperado=3000
+            assert precioEsperado == Flights.precio_total
+            assert DestinosEsperados == Flights.destinos
+            assert VuelosEsperados == Flights.vuelos
     
 
 if __name__ == "__main__":
