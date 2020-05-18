22409261da72b46eb9b82bfb7b9d4a77d960fa55
cristiangutierrz <cristian.gutierrezg@e-campus.uab.cat>
2020-05-18 10:29:56 +0200

    solapament payment

--- src/PaymentData.py
+++ src/PaymentData.py
@@ -1,29 +1,29 @@
-from src.PaymentData import PaymentData
-import unittest
+class PaymentData:
 
-class TestPaymentV1(unittest.TestCase):
+    def __init__(self, titular, num_tarjeta, cvc, method, import_):
+        self.titular = titular
+        self.num_tarjeta = num_tarjeta
+        self.cvc = cvc
+        self.method = method
+        self.import_ = import_
+        pass
     
-    def test_valid_data(self):
-        payment = PaymentData("Pepe", "4323 1234 5478 9123", "123", "VISA", "321")
-        valid = PaymentData.validar_datos(payment)
-        assert valid == True, "Invalid Data"
+    def validar_datos(self):
 
-    def test_invalid_method(self):
-        payment = PaymentData("Pepe", "4323 1234 5478 9123", "123", "visa", "321")
-        valid = PaymentData.validar_datos(payment)
-        assert valid == False, "Invalid Method"
-
-    def test_invalid_import(self):
-        payment = PaymentData("Pepe", "4323 1234 5478 9123", "123", "VISA", "-321")
-        valid = PaymentData.validar_datos(payment)
-        assert valid == False, "Invalid Negative Import"
-
-    def test_empty_data(self):
-        payment = PaymentData("", "", "", "", "")
-        valid = PaymentData.validar_datos(payment)
-        assert valid == False, "Invalid data, data is empty"
-
-if __name__ == "__main__":
-    unittest.main()
-
+        if self.method != "VISA" or self.method != "MASTERCARD":
+            return False
+        
+        # Negative Imports
+        if int(self.import_) < 0:
+            return False
+        
+        # Empty elements
+        if not self.titular or not self.num_tarjeta or not self.cvc or not self.method or not self.import_:
+            return False
+        
+        # Non-string elements
+        if not all(isinstance(element, str) for element in [self.titular, self.num_tarjeta, self.cvc, self.method, self.import_]):
+            return False
+    
+        return True
 
