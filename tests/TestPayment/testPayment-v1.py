70b7cdb1725c7e01b904f8b26c07028deb251902
cristiangutierrz <cristian.gutierrezg@e-campus.uab.cat>
2020-05-20 10:48:57 +0200

    conda executar tests

--- tests/TestPayment/testPayment-v1.py
+++ tests/TestPayment/testPayment-v1.py
@@ -1,27 +1,32 @@
-from src.PaymentData import PaymentData
+import PaymentData
 import unittest
 
 class TestPaymentV1(unittest.TestCase):
     
     def test_valid_data(self):
-        payment = PaymentData("Pepe", "4323 1234 5478 9123", "123", "VISA", "321")
-        valid = PaymentData.validar_datos(payment)
+        payment = PaymentData.PaymentData("Pepe", "4323 1234 5478 9123", "123", "VISA", "321")
+        valid = PaymentData.PaymentData.validar_datos(payment)
         assert valid == True, "Invalid Data"
 
     def test_invalid_method(self):
-        payment = PaymentData("Pepe", "4323 1234 5478 9123", "123", "visa", "321")
-        valid = PaymentData.validar_datos(payment)
+        payment = PaymentData.PaymentData("Pepe", "4323 1234 5478 9123", "123", "visa", "321")
+        valid = PaymentData.PaymentData.validar_datos(payment)
         assert valid == False, "Invalid Method"
 
     def test_invalid_import(self):
-        payment = PaymentData("Pepe", "4323 1234 5478 9123", "123", "VISA", "-321")
-        valid = PaymentData.validar_datos(payment)
+        payment = PaymentData.PaymentData("Pepe", "4323 1234 5478 9123", "123", "VISA", "-321")
+        valid = PaymentData.PaymentData.validar_datos(payment)
         assert valid == False, "Invalid Negative Import"
 
     def test_empty_data(self):
-        payment = PaymentData("", "", "", "", "")
-        valid = PaymentData.validar_datos(payment)
+        payment = PaymentData.PaymentData("", "", "", "", "")
+        valid = PaymentData.PaymentData.validar_datos(payment)
         assert valid == False, "Invalid data, data is empty"
+        
+    def test_empty_types(self):
+        payment = PaymentData.PaymentData("Pepe", "4323 1234 5478 9123", 123, "VISA", 321)
+        valid = PaymentData.PaymentData.validar_datos(payment)
+        assert valid == False, "Invalid data types"
 
 if __name__ == "__main__":
     unittest.main()
