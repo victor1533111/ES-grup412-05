a145c2397a6df0a221f4a85ff3dcf3f88f5458a4
victor1533111 <cuentaplay7778@gmail.com>
2020-05-22 17:55:41 +0200

    car_list 2.0

--- src/Cars.py
+++ src/Cars.py
@@ -2,13 +2,13 @@ from src.User import User
 from src.Rentalcars import Rentalcars
 class Cars:
 
-    def __init__(self,codigo,marca,precio,lugar_recojida,duaracion_reserva):
+    def __init__(self,codigo,marca,precio,lugar_recojida,duracion_reserva):
 
         self.codigo=codigo
         self.marca = marca
         self.lugar_recojida=lugar_recojida
-        self.duracion_reserva =duaracion_reserva
-        self.precio_total = precio * duaracion_reserva
+        self.duracion_reserva =duracion_reserva
+        self.precio_total = precio * duracion_reserva
 
         pass
 
